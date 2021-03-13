6e2c83d5d50f639c081941142c979f56953113b6
printSamarth <itssp1998@gmail.com>
2021-03-13 11:11:16 +0530

    Changes in test and code file

--- src/main/java/Calculator/Calc.java
+++ src/main/java/Calculator/Calc.java
@@ -3,6 +3,7 @@ package Calculator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.text.DecimalFormat;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -18,15 +19,15 @@ public class Calc {
         Calc calculator = new Calc();
         Scanner scanner = new Scanner(System.in);
 
-        do {
-            System.out.println("Enter the operation that you want to perform");
-            System.out.print("Press 1 to find square root\nPress 2 to Factorial\nPress 3 to Natural Log\nPress 4 to Power\n" +
-                    "Press any other key to exit\nEnter your choice: ");
+        while(true) {
+            System.out.println("To find Natural log press 1.\nTo find square root press 2.\n"+
+                    "To find Factorial press 3.\nTo find Power press 4.\nPress 5 to Exit.\n");
+            System.out.println("Enter your choice:");
             int choice;
             try {
                 choice = scanner.nextInt();
             } catch (InputMismatchException error) {
-                System.out.println("Thank you for using the calculator");
+                System.out.println("Invalid input.");
                 return;
             }
 
@@ -40,9 +41,9 @@ public class Calc {
                         logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
-                    System.out.println("================================================");
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
                     System.out.println("Square root is: " + calculator.squareRoot(num));
-                    System.out.println("================================================");
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
                     break;
                 case 2:
                     try {
@@ -52,9 +53,9 @@ public class Calc {
                         logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
-                    System.out.println("================================================");
-                    System.out.println("Factorial is: " + calculator.factorial(num));
-                    System.out.println("================================================");
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    System.out.println("Square root is: " + calculator.factorial(num));
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
                     break;
                 case 3:
                     try {
@@ -64,47 +65,52 @@ public class Calc {
                         logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
-                    System.out.println("================================================");
-                    System.out.println("Natural log is : " + calculator.naturalLog(num));
-                    System.out.println("================================================");
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    System.out.println("Square root is: " + calculator.naturalLog(num));
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
                     break;
                 case 4:
-                    double num2;
+                    double num1;
                     try {
                         System.out.print("Enter the first number : ");
                         num = scanner.nextDouble();
                         System.out.print("Enter the second number : ");
-                        num2 = scanner.nextDouble();
+                        num1 = scanner.nextDouble();
                     } catch (InputMismatchException error) {
                         logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
-                    System.out.println("================================================");
-                    System.out.println("Power result is : " + calculator.power(num, num2));
-                    System.out.println("================================================");
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    System.out.println("Square root is: " + calculator.power(num, num1));
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
                     break;
                 default:
-                    System.out.println("================================================");
-                    System.out.println("Thank you for using the calculator");
-                    System.out.println("================================================");
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    System.out.println("THANK YOU!");
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
                     return;
             }
-        } while (true);
+        }
 
     }
 
-    public double power(double num, double num2) {
+    public double power(double num1, double num2) {
         double result;
-        logger.info("Calculating Power  of two numbers " + num + " and " + num2);
-        result = Math.pow(num, num2);
-        logger.info("Result of power is : " + result);
-        return Math.pow(num, num2);
+        logger.info("Calculate power of " + num1 + " and " + num2);
+        if(num1 == 0 && num2 == 0){
+            result = Double.NaN;
+        }
+        else {
+            result = Math.pow(num1, num2);
+        }
+        logger.info("Result of power operations is : " + result);
+        return result;
     }
 
     public double naturalLog(double num) {
         double result = 0;
         try{
-            logger.info("Calculating Natural log of " + num);
+            logger.info("Calculate Natural log of " + num);
             if(num <= 0){
                 result = Double.NaN;
                 throw new ArithmeticException("Case of NaN log of <= 0");
@@ -117,7 +123,7 @@ public class Calc {
             logger.error("Number cannot be negative " + err.getMessage());
         }
         finally {
-            logger.info("Result of naturalLog is : " + result);
+            logger.info("Result of naturalLog operation is : " + result);
         }
         return result;
     }
@@ -125,6 +131,7 @@ public class Calc {
     public double factorial(double num) {
         double result = 1;
         try{
+            logger.info("Calculate factorial of " + num);
             if(num < 0) {
                 result = Double.NaN;
                 throw new ArithmeticException("Case of NaN factorial if < 0");
@@ -140,7 +147,7 @@ public class Calc {
             logger.error("Number cannot be negative " + err.getMessage());
         }
         finally {
-            logger.info("Result of factorial is: " + result);
+            logger.info("Result of factorial operation is: " + result);
         }
 
         return result;
@@ -149,18 +156,19 @@ public class Calc {
     public double squareRoot(double num) {
         double result = 0;
         try{
-            logger.info("Calculating Square root  of number " + num);
+            logger.info("Calculate Square root  of number " + num);
             if(num < 0){
                 result = Double.NaN;
                 throw new ArithmeticException("Case of NaN squareRoot of < 0");
             }
             else{
                 result = Math.sqrt(num);
+
             }
         }catch(ArithmeticException err){
             logger.error("Number cannot be negative " + err.getMessage());
         }finally {
-            logger.info("Result of squareRoot is : " + result);
+            logger.info("Result of squareRoot operation is : " + result);
         }
 
         return result;
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,54 +9,55 @@ public class CalculatorTest {
 
     @Test
     public void squareRootTruePositive(){
-        assertEquals("Square Root of number for True Positive", 3.0, cal.squareRoot(9), DELTA);
-        assertEquals("Square Root of number for True Positive", 4.0, cal.squareRoot(16), DELTA);
-        assertEquals("Square Root of number for True Positive", Double.NaN, cal.squareRoot(-10), DELTA);
+        assertEquals("Square Root of number for True Positive", 5.0, cal.squareRoot(25), DELTA);
+        assertEquals("Square Root of number for True Positive", 25.0, cal.squareRoot(625), DELTA);
+        assertEquals("Square Root of number for True Positive", 11.160645142642965, cal.squareRoot(124.56), DELTA);
+        assertEquals("Square Root of number for True Positive", Double.NaN, cal.squareRoot(-100), DELTA);
     }
 
     @Test
     public void squareRootFalsePositive(){
-        assertNotEquals("Square Root of number for False Positive", 4.0, cal.squareRoot(10), DELTA);
-        assertNotEquals("Square Root of number for False Positive", 5.0, cal.squareRoot(16), DELTA);
-        assertNotEquals("Square Root of number for False Positive", 5.0, cal.squareRoot(-16), DELTA);
+        assertNotEquals("Square Root of number for False Positive", 11.16, cal.squareRoot(124.56), DELTA);
+        assertNotEquals("Square Root of number for False Positive", 1.0, cal.squareRoot(-1), DELTA);
     }
 
     @Test
     public void factorialTruePositive(){
-        assertEquals("Factorial of number for True positive", 6.0,cal.factorial(3), DELTA);
-        assertEquals("Factorial of number for True positive", 120.0,cal.factorial(5), DELTA);
+        assertEquals("Factorial of number for True positive", 3628800,cal.factorial(10), DELTA);
+        assertEquals("Factorial of number for True positive", 120,cal.factorial(5), DELTA);
     }
 
     @Test
     public void factorialFalsePositive(){
-        assertNotEquals("Factorial of number for False positive", 8.0,cal.factorial(3), DELTA);
-        assertNotEquals("Factorial of number for False positive", 60.0,cal.factorial(5), DELTA);
+        assertNotEquals("Factorial of number for False positive", 3628801,cal.factorial(10), DELTA);
+        assertNotEquals("Factorial of number for False positive", 60,cal.factorial(5), DELTA);
     }
 
     @Test
     public void naturalLogTruePositive(){
-        assertEquals("Factorial of number for True positive", 2.833213344056216,cal.naturalLog(17), DELTA);
-        assertEquals("Factorial of number for True positive", 1.791759469228055,cal.naturalLog(6), DELTA);
-        assertEquals("Factorial of number for True positive", Double.NaN, cal.naturalLog(0), DELTA);
+        assertEquals("Factorial of number for True positive", 1.3862943611198906,cal.naturalLog(4), DELTA);
+        assertEquals("Factorial of number for True positive", 2.0794415416798357,cal.naturalLog(8), DELTA);
+
     }
 
     @Test
     public void naturalLogFalsePositive(){
-        assertNotEquals("Factorial of number for False positive", 2.833213344056216,cal.naturalLog(14), DELTA);
-        assertNotEquals("Factorial of number for False positive", 1.791759469228055,cal.naturalLog(9), DELTA);
+        assertNotEquals("Factorial of number for False positive", 2,cal.naturalLog(4), DELTA);
+        assertNotEquals("Factorial of number for False positive", 3,cal.naturalLog(8), DELTA);
         assertNotEquals("Factorial of number for False positive", 0, cal.naturalLog(0), DELTA);
     }
 
     @Test
     public void powerTruePositive(){
-        assertEquals("Factorial of number for True positive", 4.0,cal.power(2, 2), DELTA);
-        assertEquals("Factorial of number for True positive", 27.0,cal.power(3,3), DELTA);
+        assertEquals("Factorial of number for True positive", 1024,cal.power(2, 10), DELTA);
+        assertEquals("Factorial of number for True positive", 100,cal.power(10,2), DELTA);
+        assertEquals("Factorial of number for True positive", Double.NaN,cal.power(0,0), DELTA);
     }
 
     @Test
     public void powerFalsePositive(){
-        assertNotEquals("Factorial of number for False positive", 4.0,cal.power(2, 3), DELTA);
-        assertNotEquals("Factorial of number for False positive", 5.0,cal.power(3,3), DELTA);
+        assertNotEquals("Factorial of number for False positive", 7,cal.power(2, 3), DELTA);
+        assertNotEquals("Factorial of number for False positive", 1,cal.power(0,0), DELTA);
     }
 
 }
